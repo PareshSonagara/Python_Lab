918e713d6367e97a5905b0d95efb7260f884eecd
Paresh Sonagara <pmsonagara007@gmail.com>
2025-01-29 12:46:13 +0530

    absolute_value.py

--- Lab 2/absolute_value.py
+++ Lab 2/absolute_value.py
@@ -1,4 +1,8 @@
 num = int(input("Enter a number: "))
-absolute_value = abs(num)
+
+if num < 0:
+    absolute_value = -num
+else:
+    absolute_value = num
 
 print("Absolute value:", absolute_value)
